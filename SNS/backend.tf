1f7bb6ded4096794feed5ffb8fddfc0aca7319fc
massivenk <69747376+massivenk@users.noreply.github.com>
2024-05-01 11:00:53 +0200

    Update backend.tf

--- SNS/backend.tf
+++ SNS/backend.tf
@@ -1,8 +1,14 @@
+provider "aws" {
+  alias  = "backupregion"
+  region = "eu-west-1"
+}
+
 terraform {
   required_providers {
 aws = {
    source  = "hashicorp/aws"
-   version = "~> 4.18.0" }
+   version = "~> 4.18.0"
+   region = af-south-1}
    }
 }
   backend "s3" {
