794ed999a551e618348b4956349e68eb5d129c1b
massivenk <69747376+massivenk@users.noreply.github.com>
2024-05-01 11:08:44 +0200

    Update variables.tf

--- SNS/variables.tf
+++ SNS/variables.tf
@@ -4,3 +4,11 @@ variable "region" {
 variable "backupregion" {
   default = eu-west-1
 }
+variable "aws_regions" {
+  type    = list(string)
+  default = ["af-south-1", "eu-west-1", "eu-central-1"]
+}
+variable "aws_sns_topic_name" {
+  type    = string
+  default = "massive_topic_labs_sns"
+}
